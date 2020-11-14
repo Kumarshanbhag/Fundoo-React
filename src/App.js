e15c852d5cab4b9048c5bb4cf12ec7fc7bd76dbf
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-11-14 20:26:58 +0530

    ADDED : Forgot Password Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import React, {Component} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './Components/SignIn';
+import ForgotPassword from './Components/ForgotPassword';
 
 class App extends Component {
   render() {
@@ -9,7 +10,8 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <Switch>
-           <Route path='/' component={SignIn}></Route>
+           <Route path='/' component={SignIn} exact={true}></Route>
+           <Route path='/forgotpassword' component={ForgotPassword}></Route>
          </Switch>
       </BrowserRouter>
       </div>
---
+++ src/Components/ForgotPassword.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import FundooLogo from './FundooLogo';
+import { Card,
+    CardContent,
+    Typography,
+    TextField,
+    Grid,
+    Button, 
+}
+from "@material-ui/core";
+import "../scss/forgotpassword.scss";
+import { Link } from "react-router-dom";
+
+class ForgotPassword extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            emailId:null
+        };
+        this.handleChange = this.handleChange.bind(this);
+      }
+      handleChange = async (e) => {
+        // const { name } = e.target;
+        this.setState({ [e.target.name]: await e.target.value });
+      };
+
+    render(){
+        return(
+            <Card className="mainLogo" justify="center" boxShadow={3}>
+                <CardContent>
+                    <FundooLogo /> 
+                    <Typography className="signInLabel" m={3} variant="h5">
+                        Find your email
+                    </Typography>
+                    <Typography className="loginInfo">Enter your recovery email</Typography> 
+
+                    <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+                    
+                      <TextField className="EmailInput" value={this.state.emailId} name="EmailId" onChange="handleChange" fullWidth="true" 
+                      id="standard-basic" color="secondary" label="Email Id *"/>
+                    
+                    </Grid> 
+
+                    <Link className="goBackButton" to="/">
+                        Back
+                    </Link>
+                    <Button className="findEmail">
+                        Next
+                    </Button>
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default ForgotPassword;
