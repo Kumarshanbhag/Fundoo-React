8515e37278448cd41673d7925c496c228e5b575c
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-11-17 06:01:05 +0530

    ADDED : Validation For SignUp Page

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -10,12 +10,19 @@ import { Card,
     FormControl,
     InputLabel,
     OutlinedInput,
+    FormHelperText
 }
 from "@material-ui/core"; 
 import "../scss/signup.scss";
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import { Link } from "react-router-dom";
+
+const NAME_PATTERN = RegExp("^[A-Z]{1}[a-zA-Z]{2,}$");
+const EMAIL_PATTERN = RegExp(
+    "^[a-zA-Z0-9]+[.+_-]?[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]{0,3}"
+  );
+const CONTACT_PATTERN = RegExp("^[0-9]{10}$");
 
 class SignUp extends React.Component{
     constructor() {
@@ -28,13 +35,49 @@ class SignUp extends React.Component{
             password:null,
             confirm:null,
             passwordVisibility:true,
+            validName:false,
+            validLastname:false,
+            validEmail:false,
+            validContact:false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleVisibility=this.handleVisibility.bind(this);
       }
 
       handleChange = async (e) => {
+        const { name } = e.target;
         this.setState({ [e.target.name]: await e.target.value });
+        console.log(name);
+        switch (name) {
+            case "firstname":
+              NAME_PATTERN.test(this.state.firstname)
+                ? this.setState({ validName: true })
+                : this.setState({ validName: false });
+            break;
+
+            case "lastname":
+                NAME_PATTERN.test(this.state.lastname)
+                  ? this.setState({ validLastname: true })
+                  : this.setState({ validLastname: false });
+            break;
+
+            case "emailId":
+                EMAIL_PATTERN.test(this.state.emailId)
+                  ? this.setState({ validEmail: true })
+                  : this.setState({ validEmail: false });
+            break;
+
+            case "mobile":
+                CONTACT_PATTERN.test(this.state.mobile)
+                  ? this.setState({ validContact: true })
+                  : this.setState({ validContact: false });
+            break;
+    
+            default:
+              break;
+        }
+        console.log(this.state.validName);
+        console.log(this.state.validLastname);
       };
 
       handleVisibility = () => {
@@ -55,27 +98,47 @@ class SignUp extends React.Component{
                         Create your Fundoo Account
                     </Typography>
                     
-                    <Grid className="gridContainer" container spacing={1} direction="row" justify="center" 
+                    <Grid className="gridContainer" container spacing={2} direction="row" justify="center" 
                     alignItems="center" spacing={2}>
                         
                         <Grid item xs={6} className="gridItem">
                             <TextField name='firstname' onChange={this.handleChange} value={this.state.firstname}
                             fullWidth="true" id="outlined-basic" label="First name*" variant="outlined"/>
+                            {this.state.validName === false ? (
+                                <FormHelperText style={{ color: "red" }} id="outlined-weight-helper-text">
+                                    Enter Valid First Name
+                                </FormHelperText>
+                            ) : null} 
                         </Grid>
                         
                         <Grid item xs={6}  className="gridItem">
                             <TextField name="lastname" onChange={this.handleChange} value={this.state.lastname} fullWidth="true" 
                             id="outlined-basic" label="Last name*" variant="outlined"/>
+                            {this.state.validLastname === false ? (
+                                <FormHelperText style={{ color: "red" }} id="outlined-weight-helper-text">
+                                    Enter Valid Last Name
+                                </FormHelperText>
+                            ) : null} 
                         </Grid>
                         
                         <Grid item xs={12} className="gridItem">
                             <TextField name='emailId' onChange={this.handleChange} value={this.state.emailId} fullWidth="true"
                              id="outlined-basic" label="Email Id*" variant="outlined"/>
+                             {this.state.validEmail === false ? (
+                                <FormHelperText style={{ color: "red" }} id="outlined-weight-helper-text">
+                                    Enter Valid Email Id
+                                </FormHelperText>
+                            ) : null} 
                         </Grid>
 
                         <Grid item xs={12} className="gridItem">
                             <TextField name='mobile' onChange={this.handleChange} value={this.state.mobile} fullWidth="true"
                              id="outlined-basic" label="Mobile*" variant="outlined"/>
+                             {this.state.validContact === false ? (
+                                <FormHelperText style={{ color: "red" }} id="outlined-weight-helper-text">
+                                    Enter Valid Last Name
+                                </FormHelperText>
+                            ) : null}
                         </Grid>
                         
                         <Grid item xs={6} className="gridItem">
