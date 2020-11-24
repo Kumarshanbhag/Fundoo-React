f4f3a2993b3d85a646bead6e51e8e26eab70c89a
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-11-24 09:02:59 +0530

    ADDED : Media Query To Make Sidebar Responsive

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,34 +6,14 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  createMuiTheme,
-  ThemeProvider
 } from "@material-ui/core";
 import ArchiveIcon from '@material-ui/icons/Archive';
 import ReminderIcon from '@material-ui/icons/Notifications';
 import TrashIcon from '@material-ui/icons/Delete';
 import NoteIcon from '@material-ui/icons/EmojiObjects';
-import EditIcon from '@material-ui/icons/EditOutlined'
+import EditIcon from '@material-ui/icons/EditOutlined';
 
-const theme = createMuiTheme({
-  overrides: {
-      MuiDrawer: {
-          paper: {
-              top: '9%',
-              width: '15%'
-          },
-          paperAnchorDockedRight:{
-            borderRight: 20
-          }
-      },
-      MuiPaper: {
-          root: {
-              backgroundColor: 'transparent'
-          }
-      }
-
-  }
-})
+import '../scss/sidebar.scss';
 
 class Sidebar extends Component {
   constructor(props){
@@ -41,7 +21,6 @@ class Sidebar extends Component {
   }
   render() {
     return (
-      <ThemeProvider  theme={theme}>
         <Drawer variant='persistent' open={this.props.openDrawer} >
           <List>
             <ListItem className="note">
@@ -82,7 +61,6 @@ class Sidebar extends Component {
             </ListItem>
           </List>
         </Drawer>
-      </ThemeProvider>
     )
   }
 }
